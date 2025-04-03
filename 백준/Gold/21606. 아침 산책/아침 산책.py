8598f06958bef5d004b53371c4e20bdbdd2ed7a3
yechan928 <cksdid1363@gmail.com>
2025-04-04 00:18:45 +0900

    [Gold III] Title: 아침 산책, Time: 792 ms, Memory: 105764 KB, Score: 200 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -1,44 +1,44 @@
 import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
 
-n = int(sys.stdin.readline())
-A = sys.stdin.readline().strip()
+n = int(input())
+
+A = input().rstrip()
 
 graph = [[] for _ in range(n+1)]
-place = [0]*(n+1)                   # 실내인지 실외인지 판단하는 배열, 실내:1,실외:0
-visited = [0]*(n+1)
+place = [0] * (n+1)
+visited = [0] * (n+1)
 
 for i in range(len(A)):
-    if A[i] ==1:
-        place[i+1]=1
+    if A[i] == '1':
+        place[i+1] = 1
 
-for _ in range(n-1):            #트리에서 간선의개수  = 정점의 개수-1
-    u, v = list(map(int, sys.stdin.readline().split()))
-    graph[u].append(v)
-    graph[v].append(u)
-
-
-#dfs를 생각하자
-def dfs(node):          # 한 트리내에서 실내 개수 세기
-    res = 0
+for _ in range(n-1):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+    
+def dfs(node):
+    res = 0 
     for next_node in graph[node]:
-        if place[next_node]==0:                 # 인접한 노드의 장소가 실외이면 
-            if not visited[next_node] :         # 인접한 노드를 방문했는지 확인하고 방문 안했으면
-                visited[next_node]=1            # 방문했다고 체크해주고
-                res += dfs(next_node)           # 재귀를 통해 연결된 실외 개수 탐색
+        if place[next_node] == 0:
+            if not visited[next_node]:
+                visited[next_node] = 1
+                res += dfs(next_node)
         else:
-            res += 1                            # 인접한 노드가 실내면  개수 +1
-    return res 
+            res += 1
+    return res
 
-ans = 0     # 최종 정답
+ans = 0
 for i in range(1, n+1):
-    if place[i] ==0:                    # 현재 노드가 실외이면
-        if not visited[i]:              # 아직 방문하지 않았으면
-            visited[i] = 1              # 방문 표시 
-            res = dfs(i)                # 이 실외 영역에 연결된  실내 노드 수 계산 
-            ans += res*(res-1)          # 순열로 계산
-    else:                               # 현재 노드가 실내면
-        for next_node in graph[i]:      # 인접한 모든 노드 확인하고
-            if place[next_node] ==1:    # 실내면 +1을 해라
-                ans +=1
-
+    if place[i] == 0:
+        if not visited[i]:
+            visited[i] = 1
+            res = dfs(i)
+            ans += res * (res - 1)
+    else:
+        for next_node in graph[i]:
+            if place[next_node] == 1:
+                ans += 1
 print(ans)
