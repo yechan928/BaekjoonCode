ed5cd28f68e9ca5c868aed2b87e08569227b6be8
yechan928 <cksdid1363@gmail.com>
2025-09-10 10:36:17 +0900

    [Silver I] Title: 단지번호붙이기, Time: 36 ms, Memory: 32488 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,35 +1,38 @@
 import sys
-n = int(sys.stdin.readline())
-board =[list(map(int,sys.stdin.readline().strip())) for _ in range(n)]
-visited=[[False]*n for _ in range(n)]
 
-dx = [0,-1,0,1]
-dy = [1,0,-1,0]
+n = int(sys.stdin.readline())
+board = [list(map(int, sys.stdin.readline().strip())) for _ in range(n)]
+visited = [[False] * n for _ in range(n)]
+
+dx = [1, 0, -1, 0]
+dy = [0, -1, 0, 1]
 danji = 0
-def dfs(x,y):
+
+
+def dfs(x, y):
     global danji
-    visited[x][y]=True
+    visited[x][y] = True
     danji += 1
 
     for i in range(4):
-        nx = x+dx[i]
-        ny = y+dy[i]
+        nx = x + dx[i]
+        ny = y + dy[i]
 
-        if 0 <= nx < n and 0<= ny <n:
-            if not visited[nx][ny] and board[nx][ny]==1 :
-                dfs(nx,ny)
+        if 0 <= nx < n and 0 <= ny < n:
+            if board[nx][ny] and not visited[nx][ny]:
+                dfs(nx, ny)
     return danji
+
 
 result = []
 count = 0
 for i in range(n):
     for j in range(n):
-        if board[i][j]==1 and not visited[i][j]:
-            result.append(dfs(i,j))
+        if board[i][j] and not visited[i][j]:
+            result.append(dfs(i, j))
             danji = 0
-            count+=1
-
-result.sort()
+            count += 1
 print(count)
-for i in result:
-    print(i)
+result.sort()
+for i in range(count):
+    print(result[i])
